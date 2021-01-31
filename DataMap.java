15dbda4adf0fd698e0fc40c572beb407c4229c02
zedyang59 <61482779+zedyang59@users.noreply.github.com>
2021-01-31 11:31:33 -0800

    Add files via upload

---
+++ DataMap.java
@@ -0,0 +1,18 @@
+package spacex;
+
+public class DataMap {
+	
+	String name;
+	String value;
+	
+	public DataMap() {
+		this("","");
+	}
+
+	public DataMap(String name, String value) {
+		
+		this.name = name;
+		this.value = value;
+	}
+
+}
